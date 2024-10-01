b63a2a34155cf471aa97ed1b6de14c49faf8d951
Oli <oli@olillin.com>
2024-10-01 20:12:58 +0200

    Fix bonk

--- src/main/kotlin/dev/svaren/bonk/Bonk.kt
+++ src/main/kotlin/dev/svaren/bonk/Bonk.kt
@@ -10,14 +10,21 @@ import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
 import net.minecraft.util.ActionResult
-import net.minecraft.village.TradeOfferList
+import net.minecraft.village.VillagerProfession
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+
 class Bonk : ModInitializer {
+    private val logger: Logger = LoggerFactory.getLogger("BONK")
+
     override fun onInitialize() {
         AttackEntityCallback.EVENT.register(fun(player, world, hand, entity, _): ActionResult {
-            if (!player.getStackInHand(hand)
-                    .isIn(ItemTags.SHOVELS) || entity.type != EntityType.VILLAGER || world.server == null
+            val handItem = player.getStackInHand(hand)
+
+            if (!handItem.isIn(ItemTags.SHOVELS)
+                || entity.type != EntityType.VILLAGER
+                || world.server == null
             ) {
                 return ActionResult.PASS
             }
@@ -25,25 +32,44 @@ class Bonk : ModInitializer {
             val villager = entity as VillagerEntity
             val serverWorld = world as ServerWorld
 
+            val canBeBonked: Boolean = villager.experience == 0
+                    && villager.villagerData.profession != VillagerProfession.NONE
+            if (!canBeBonked) {
+                serverWorld.spawnParticles(
+                    ParticleTypes.ANGRY_VILLAGER,
+                    entity.x,
+                    entity.y + 1.5,
+                    entity.z,
+                    1,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.01
+                )
+                serverWorld.playSoundFromEntity(villager, SoundEvents.BLOCK_NOTE_BLOCK_COW_BELL.value(), SoundCategory.NEUTRAL, 1f, 0f)
+
+                return ActionResult.FAIL
+            }
+
             serverWorld.spawnParticles(
-                ParticleTypes.EXPLOSION,
+                ParticleTypes.POOF,
                 entity.x,
-                entity.y + 1,
+                entity.y + 1.5,
                 entity.z,
-                10,
-                0.0,
-                0.0,
-                0.0,
-                0.0
+                8,
+                0.2,
+                0.2,
+                0.2,
+                0.01
             )
-            player.playSoundToPlayer(SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.NEUTRAL, 1f, 0.5f)
 
-            villager.damage(world.damageSources.generic(), 0f)
-            villager.offers = TradeOfferList()
+            serverWorld.playSoundFromEntity(villager, SoundEvents.BLOCK_NOTE_BLOCK_COW_BELL.value(), SoundCategory.NEUTRAL, 1f, 0.7f)
 
-            return ActionResult.SUCCESS
+            villager.offers = null
+
+            return ActionResult.FAIL
         })
 
-        LoggerFactory.getLogger("BONK").info("Initialized!")
+        logger.info("Initialized!")
     }
 }
